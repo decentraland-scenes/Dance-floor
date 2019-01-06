b7f7cd59af08a10cdb7758450419f5ac90af7261
Nico E <earnshaw.nico@gmail.com>
2019-01-06 13:20:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Dance floor
+
+A scene with animations, sound, and tiles on the floor that randomly change color to the beat.
+
+- audio
+- glTF models
+- animations
+- materials
+- custom components 
+- component groups
+
+![](screenshot/screenshot.png)
+
+<!--
+[Explore the scene](): this link takes you to a copy of the scene deployed to a remote server where you can interact with it just as if you were running `dcl start` locally.
+-->
+
+**Install the CLI**
+
+Download and install the Decentraland CLI by running the following command
+
+```bash
+npm i -g decentraland
+```
+
+For a more details, follow the steps in the [Installation guide](https://docs.decentraland.org/documentation/installation-guide/).
+
+
+**Previewing the scene**
+
+Once you've installed the CLI, download this example and navigate to its directory from your terminal or command prompt.
+
+_from the scene directory:_
+
+```
+$:  dcl start
+```
+
+Any dependencies are installed and then the CLI will open the scene in a new browser tab automatically.
+
+
+Learn more about how to build your own scenes in our [documentation](https://docs.decentraland.org/) site.
---
+++ src/game.ts
@@ -0,0 +1,110 @@
+
+
+import { playSound } from '@decentraland/SoundController'
+
+const colors = ["#1dccc7", "#ffce00", "#9076ff", "#fe3e3e", "#3efe94", "#3d30ec", "#6699cc"]
+
+///////////////////////////////
+// Custom components
+
+@Component('tileFlag')
+export class TileFlag {
+}
+
+@Component('beat')
+export class Beat {
+  interval: number
+  timer: number
+  constructor(interval: number = 0.5){
+    this.interval = interval
+    this.timer = interval
+  }
+}
+
+
+///////////////////////////
+// Entity groups
+
+const tiles = engine.getComponentGroup(TileFlag)
+
+///////////////////////////
+// Systems
+
+export class changeColor implements ISystem {
+  update(dt: number) {
+    let beat = beatKeeper.get(Beat)
+    beat.timer -= dt
+    if (beat.timer < 0){
+      beat.timer = beat.interval
+      for (let tile of tiles.entities) {
+        const colorNum = Math.floor(Math.random() * colors.length)
+        tile.set(tileMaterials[colorNum])
+      }
+    }
+  }
+}
+
+
+engine.addSystem(new changeColor)
+
+///////////////////////////
+// INITIAL ENTITIES
+
+
+// Create materials
+let tileMaterials = []
+for (let i = 0; i < colors.length; i ++){
+  let material = new Material()
+  material.albedoColor = Color3.FromHexString(colors[i])
+  tileMaterials.push(material)
+}
+
+// Add Tiles
+[0, 1, 2, 3, 4].forEach(x => {
+  [0, 1, 2, 3, 4].forEach(z => {
+    const tile = new Entity()
+    tile.add(new PlaneShape())
+    tile.add(new Transform({
+      position: new Vector3((x * 2) + 1, 0, (z * 2) + 1),
+      rotation: Quaternion.Euler(90, 0, 0),
+      scale: new Vector3(2, 2, 2)
+    }))
+    tile.add(new TileFlag())
+    const colorNum = Math.floor(Math.random() * colors.length)
+    tile.set(tileMaterials[colorNum])
+    engine.addEntity(tile)
+  })
+})
+
+
+// Add dancing Trevor
+const trevor = new Entity()
+trevor.add(new GLTFShape("models/Trevor.glb"))
+const clipDance = new AnimationClip("Armature_Idle")
+trevor.get(GLTFShape).addClip(clipDance)
+clipDance.play()
+trevor.add(new Transform({
+  position: new Vector3(5, 0.1, 5),
+  rotation: Quaternion.Euler(0, -90, 0),
+  scale: new Vector3(1.5, 1.5, 1.5)
+}))
+
+engine.addEntity(trevor)
+
+// Singleton to keep track of the beat
+let beatKeeper = new Entity()
+beatKeeper.add(new Beat(0.5))
+
+
+//Play music
+executeTask(async () => {
+  try {
+    await playSound("sounds/Vexento.ogg", {
+      loop: true,
+      volume: 75,
+    })
+  } catch {
+    log('failed to play sound')
+  }
+})
+
