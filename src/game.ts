e12cab79bcc5ba09066cf4625a3bbd0f40992527
nearnshaw <nearnshaw@decentraland.org>
2022-03-24 16:48:00 -0300

    lint fix and remove package-lock.json (if aplies)

--- src/game.ts
+++ src/game.ts
@@ -1,24 +1,28 @@
-
-
-const colors = ["#1dccc7", "#ffce00", "#9076ff", "#fe3e3e", "#3efe94", "#3d30ec", "#6699cc"]
+const colors = [
+  '#1dccc7',
+  '#ffce00',
+  '#9076ff',
+  '#fe3e3e',
+  '#3efe94',
+  '#3d30ec',
+  '#6699cc'
+]
 
 ///////////////////////////////
 // Custom components
 
 @Component('tileFlag')
-export class TileFlag {
-}
+export class TileFlag {}
 
 @Component('beat')
 export class Beat {
   interval: number
   timer: number
-  constructor(interval: number = 0.5){
+  constructor(interval: number = 0.5) {
     this.interval = interval
     this.timer = interval
   }
 }
-
 
 ///////////////////////////
 // Entity groups
@@ -30,11 +34,11 @@ const tiles = engine.getComponentGroup(TileFlag)
 
 export class changeColor implements ISystem {
   update(dt: number) {
-    let beat = beatKeeper.getComponent(Beat)
+    const beat = beatKeeper.getComponent(Beat)
     beat.timer -= dt
-    if (beat.timer < 0){
+    if (beat.timer < 0) {
       beat.timer = beat.interval
-      for (let tile of tiles.entities) {
+      for (const tile of tiles.entities) {
         const colorNum = Math.floor(Math.random() * colors.length)
         tile.addComponentOrReplace(tileMaterials[colorNum])
       }
@@ -42,31 +46,31 @@ export class changeColor implements ISystem {
   }
 }
 
-
-engine.addSystem(new changeColor)
+engine.addSystem(new changeColor())
 
 ///////////////////////////
 // INITIAL ENTITIES
 
-
 // Create materials
-let tileMaterials : Material[] = []
-for (let i = 0; i < colors.length; i ++){
-  let material = new Material()
+const tileMaterials: Material[] = []
+for (let i = 0; i < colors.length; i++) {
+  const material = new Material()
   material.albedoColor = Color3.FromHexString(colors[i])
   tileMaterials.push(material)
 }
 
 // Add Tiles
-[0, 1, 2, 3].forEach(x => {
-  [0, 1, 2, 3].forEach(z => {
+;[0, 1, 2, 3].forEach((x) => {
+  ;[0, 1, 2, 3].forEach((z) => {
     const tile = new Entity()
     tile.addComponent(new PlaneShape())
-    tile.addComponent(new Transform({
-      position: new Vector3((x * 4) + 2, 0, (z * 4) + 2),
-      rotation: Quaternion.Euler(90, 0, 0),
-      scale: new Vector3(4, 4, 4)
-    }))
+    tile.addComponent(
+      new Transform({
+        position: new Vector3(x * 4 + 2, 0, z * 4 + 2),
+        rotation: Quaternion.Euler(90, 0, 0),
+        scale: new Vector3(4, 4, 4)
+      })
+    )
     tile.addComponent(new TileFlag())
     const colorNum = Math.floor(Math.random() * colors.length)
     tile.addComponent(tileMaterials[colorNum])
@@ -74,22 +78,23 @@ for (let i = 0; i < colors.length; i ++){
   })
 })
 
-
 // Add dancing Trevor
 const trevor = new Entity()
-trevor.addComponent(new GLTFShape("models/Trevor.glb"))
-const clipDance = new AnimationState("Armature_Idle")
+trevor.addComponent(new GLTFShape('models/Trevor.glb'))
+const clipDance = new AnimationState('Armature_Idle')
 const animator = new Animator()
 animator.addClip(clipDance)
 trevor.addComponent(animator)
 clipDance.play()
-trevor.addComponent(new Transform({
-  position: new Vector3(5, 0.1, 5),
-  rotation: Quaternion.Euler(0, -90, 0),
-  scale: new Vector3(1.5, 1.5, 1.5)
-}))
+trevor.addComponent(
+  new Transform({
+    position: new Vector3(5, 0.1, 5),
+    rotation: Quaternion.Euler(0, -90, 0),
+    scale: new Vector3(1.5, 1.5, 1.5)
+  })
+)
 
-const audioClip = new AudioClip("sounds/Vexento.mp3")
+const audioClip = new AudioClip('sounds/Vexento.mp3')
 audioClip.loop = true
 const audioSource = new AudioSource(audioClip)
 trevor.addComponent(audioSource)
@@ -97,8 +102,7 @@ trevor.addComponent(audioSource)
 engine.addEntity(trevor)
 
 // Singleton to keep track of the beat
-let beatKeeper = new Entity()
+const beatKeeper = new Entity()
 beatKeeper.addComponent(new Beat(0.5))
 
 audioSource.playing = true
-
